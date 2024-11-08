a02cc0c59b48e8e1fa8907d085ba015ed77665e4
CodeSpace Academy <duwaynefrieslaar23@gmail.com>
2024-11-08 15:05:15 +0200

    created component folder

---
+++ client/src/components/userprofile.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Userprofile(){
+    return(
+        <div id="profile-page">
+
+        <div id="left-section">
+        <div id="profile-board">
+          <img src="#" alt="profile pic"/>
+          <h1>Hailey Williams</h1>
+          <h2>User</h2>
+        </div>
+
+        <div id="bio-section">
+          <h1>Bio</h1>
+          <p>
+            A very passionate developer based in Capetown. Unmatched passion. 
+            I love long walks on the beach with my dog, 'tugboat'
+          </p>
+        </div>
+
+        </div>
+
+        <div id="right-section">
+            <h1>User profile</h1>
+            <h4></h4>
+        </div>
+        </div>
+    )
+}
