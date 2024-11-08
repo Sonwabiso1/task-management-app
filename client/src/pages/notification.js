9e5bab639eee459d3dd96597c3498e54814d4eb7
CodeSpace Academy <duwaynefrieslaar23@gmail.com>
2024-11-08 19:03:49 +0200

    Dominated the notification page

--- client/src/pages/notification.js
+++ client/src/pages/notification.js
@@ -1,15 +1,16 @@
 import React from "react";
+import './notification.css'
 
 export default function Notification(){
     return(
         <>
         <div id="notif-title"><h1>Notifications</h1></div>
-        <div id="notification"> 
+        <div class="notification"> 
             <p>🔔Work on the backend for the stanley banking app</p>
             <p>⏱️Due: 17 November 2024 C.O.B</p>
         </div>
         <br/>
-        <div>
+        <div class="notification">
             <p>🔔Write a proposal for the portfolio project</p>
             <p>⏱️Due: 10 November 2024 C.O.B</p>
         </div>
