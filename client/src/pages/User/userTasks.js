23ea77a0c1508747e7fe66aea6f6134bcf3c798a
Robyn011 <robyncocopumpoen@gmail.com>
2024-11-13 15:11:58 +0200

    adding the user tasks

--- client/src/pages/User/userTasks.js
+++ client/src/pages/User/userTasks.js
@@ -4,19 +4,35 @@ import '../../styles/User/userTasks.css';
 export default function Tasks() {
     return (
         <div class="board">
-    <div class="column">
-        <div class="header todo">To do</div>
-        <div class="box"></div>
-    </div>
-    <div class="column">
-        <div class="header in-progress">In Progress</div>
-        <div class="box"></div>
-        <div class="box"></div>
-    </div>
-    <div class="column">
-        <div class="header done">Done</div>
-        <div class="box"></div>
-    </div>
-</div>
+            <div class="column">
+                <div class="header todo">To do</div>
+                <div class="box">
+                    <h4>
+                     Task name
+                    </h4>
+                    <p>Task description</p>
+
+                </div>
+            </div>
+            <div class="column">
+                <div class="header in-progress">In Progress</div>
+                <div class="box">
+                    <h4>
+                      Task name
+                    </h4>
+                    <p>Task description</p>
+                </div>
+                <div class="box"></div>
+            </div>
+            <div class="column">
+                <div class="header done">Done</div>
+                <div class="box">
+                    <h4>
+                     Task name
+                    </h4>
+                    <p>Task description</p>
+                </div>
+            </div>
+        </div>
     );
 }
