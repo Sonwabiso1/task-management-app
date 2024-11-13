168ff4c43310276e3fd552049f5530cc21f22e75
name <duwaynefrieslaar23@gmail.com>
2024-11-13 13:23:43 +0200

    filled in empty pages
    I filled in the admin pages with the same content as the user pages, but with additional alterations

---
+++ client/src/pages/Admin/adminNotifications.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function AdminNotifications(){
+    return(
+        <>
+        <h1>Notifications</h1>
+        <div class="anotification"> 
+            <p>🔔Work on the backend for the stanley banking app</p>
+            <p>⏱️Due: 17 November 2024 C.O.B</p>
+        </div>
+        <br/>
+        <div class="anotification">
+            <p>🔔Write a proposal for the portfolio project</p>
+            <p>⏱️Due: 10 November 2024 C.O.B</p>
+        </div>
+        </>
+    )
+}
