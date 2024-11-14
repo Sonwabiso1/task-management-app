26dea4c593ce5c809c2f74ed370f53488a8b1060
name <duwaynefrieslaar23@gmail.com>
2024-11-14 22:04:13 +0200

    Removed userprofile
    DOne testing

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,6 @@ import UserProfile from './user-pages/userprofile';
 function App() {
   return (
     <BrowserRouter>
-    <UserProfile/>
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/signup" element={<Signup />} />
