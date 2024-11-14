a48295a67ff3f406549428d03481a8565f67e20a
Nikithamboks25 <Nikitha.mbokotwana@capaciti.org.za>
2024-11-14 11:42:12 +0200

    navbar and app.js changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,32 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
-// Landing Page imports
-import Signup from './pages/Landing Pages/Signup';
-import Home from './pages/Landing Pages/Home';
 
+//Components
 import Footer from './components/footer';
-
-//Landing Page imports
-import Home from './landing-pages/Home';
+import Navbar from './components/Navbar';
+// Landing Page imports
 import Signup from './landing-pages/Signup';
+import Home from './landing-pages/Home';
 import Login from './landing-pages/Login';
-
-import Dashboard from './pages/Dashboard';
-import Layout from './pages/Layout';
-import Projects from './pages/Projects';
-import Notifications from './pages/Notifications'
-
-function MainContent({ showSidebar, toggleSidebar }) {
-  const location = useLocation();
-  const isUserProfile = location.pathname === '/userprofile';
-
-  return (
-    <>
-      {/* Conditionally render Bars based on the current path */}
-      {!isUserProfile && <Bars toggleSidebar={toggleSidebar} showSidebar={showSidebar} />}
-      <div className={`content ${showSidebar && !isUserProfile ? 'sidebar-open' : 'sidebar-closed'}`}>
-        <Routes>
-          <Route path="/" element={<Signup />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/tasks" element={<UserTasks />} />
-          <Route path="/memberlogin" element={<MemberLog />} />
-          <Route path="/userprofile" element={<Userprofile />} />
-          <Route path="/notification" element={<UserNotifications />} />
-          <Route path="/projects" element={<UserProjects />} />
-          <Route path="/adminprofile" element={<Adminprofile />} /> */}
-      {/* </div> */}
-      <Footer />
-    </>
-  );
-}
+import Dashboard from './dashboard/Dashboard';
+import Layout from './dashboard/Layout';
+// import Projects from './pages/Projects';
+// import Notifications from './pages/Notifications'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/navbar" element={<Navbar />} />
         <Route path="/signup" element={<Signup />} />
         <Route path='/login' element={<Login />} />
         {/* Grouped routes under "/home" */}
         <Route path="/home" element={<Layout />}>
           <Route index element={<Navigate to="dashboard" />} /> {/* Redirect "/home" to "/home/dashboard" */}
           <Route path="dashboard" element={<Dashboard />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="notifications" element={<Notifications />} />
+          {/* <Route path="projects" element={<Projects />} />
+          <Route path="notifications" element={<Notifications />} /> */}
         </Route>
         
         {/* Separate route for "/logout" */}
